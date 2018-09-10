52bfbcc29855eb859d0e9e20eb7dc27f1a9667e3
Fernando Ventura Jr <fventurajr@gmail.com>
2018-09-10 12:53:45 -0300

    Add files via upload
    Acréscimo da contagem detalhada
    contendo dropdown list para as complexidades e entrada manual de RL e ID
    Maior espaçamento entre as linhas de ALI, AIE, EE, SE, CE

--- script.js
+++ script.js
@@ -23,7 +23,60 @@ function pfEstimada() {
 
 
 }
+
+function selecionaTipo() {
+
+    // esconde todas as contagens
+    var secao1 = document.getElementById("secao1");
+    var secao2 = document.getElementById("secao2");
+    /*
+    secao1.style.display="none";
+    secao1.style.visibility="hidden";
+    secao2.style.display="none";
+    secao2.style.visibility="hidden";
+    */
+    var rbTipos = document.getElementsByName("tipo");
+    var rbIndicativa = document.getElementById("Indicativa");
+    var rbEstimada   = document.getElementById("Estimada");
+    var rbEstimada   = document.getElementById("Detalhada");
+    secao2.style.display="block";
+    secao2.style.visibility="visible";
+    switch ( rbTipos.values ) {
+        case 1 :
+            secao1.style.display="block";
+            secao1.style.visibility="visible";
+            break;
+        case 2 :    
+           secao2.style.display="block";
+           secao2.style.visibility="visible";
+           break;
+        default :
+           console.log("nada selecionado!!!"); 
+    }
+
+
+}
+function Mudarestado(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none")
+        document.getElementById(el).style.display = 'block';
+    else
+        document.getElementById(el).style.display = 'none';
+}
+
+// evento para contagem de pontos de função indicativa
 var btnIndicativa = document.getElementById("btnIndicativa");
 btnIndicativa.addEventListener("click", pfIndicativa);
+
+// evento para contagem de pontos de função estimada
 var btnEstimada = document.getElementById("btnEstimada");
 btnEstimada.addEventListener("click", pfEstimada);
+
+// evento para o tipo de contagem: [ Indicativa | Estimada | Detalhada ]
+/*
+var rbIndicativa = document.getElementById("Indicativa");
+var rbEstimada   = document.getElementById("Estimada");
+var rbEstimada   = document.getElementById("Detalhada");
+var rbTipos = document.getElementsByName("tipo");
+rbTipos.addEventListener("change", selecionaTipoContagem);
+*/
